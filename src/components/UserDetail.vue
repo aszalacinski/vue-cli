275556e0aa747355e1831df508079c9aea4b4337
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-19 11:55:16 -0400

    lesson 108

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -8,7 +8,11 @@
 
 <script>
     export default {
-        props: ['myName'],
+        props: {
+            myName: String,
+            required: true,
+            default: 'Default'
+        },
         methods: {
             switchName() {
                 return this.myName.split("").reverse().join("");
