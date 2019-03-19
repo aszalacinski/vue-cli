0c108182290c8a906a4a5133aec8c66e61dd0135
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-03-19 19:44:37 -0400

    done

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,48 @@
 <template>
-  <h1>Hello World!</h1>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to delete it</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default { }
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+
+
+export default { 
+  data: function() {
+    return {
+      quotes: [
+        'Just a quote to see something'
+      ],
+      maxQuotes: 10
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+  methods: {
+    newQuote: function (quote) {
+      if(this.quotes.length >= this.maxQuotes) {
+        return alert('Please delete quotes first');
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote: function(index) {
+      this.quotes.splice(index, 1);
+    }
+  }
+}
 </script>
 
 <style>
