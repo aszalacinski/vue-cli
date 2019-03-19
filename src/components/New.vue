51fcedf2ebcfe072244f768f47aff057af517128
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-03-19 18:48:39 -0400

    lesson 127

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
         <h3>New Quote</h3>
+        <button @click="counter++">Increase!</button>
+        {{ counter }}
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data: function() {
+            return {
+                counter: 0
+            }
+        },
+        destroyed() {
+            console.log('Destroyed');
+        }
+    }
 </script>
 
 <style scoped>
