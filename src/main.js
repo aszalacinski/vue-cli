7b3773211eb9d4deccf5e60001351cea2f4cc63a
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-03-20 18:16:55 -0400

    lesson 172

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // global filter
-Vue.filter('to-lowercase');
+Vue.filter('toLowercase', function(value) {
+  return value.toLowerCase();
+});
 
 new Vue({
   el: '#app',
