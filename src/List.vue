765fbc92337cd24a2757da1357ca2d70423ab9c1
Aarron Szalacinski <aarron.szalacinski@happyappsoftware.com>
2019-03-20 18:28:29 -0400

    lesson 174

---
+++ src/List.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Filters & Mixins</h1>
+        <hr>
+        <input type="text" v-model="filterText" class="form-control">
+        <ul>
+            <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default { 
+  data() {
+    return {
+      fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+      filterText: ''
+    }
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter((element) => {
+        return element.match(this.filterText);
+      });
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
