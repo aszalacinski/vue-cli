6dd4870ec103de334cf73515522a1eeb762ec437
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-21 13:53:28 -0400

    lesson 239

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -2,6 +2,8 @@
     <div>
         <h3>Edit the User</h3>
         <p>Editing User {{ id }}</p>
+        <p>Locale: {{ $route.query.locale }}</p>
+        <p>Analytics: {{ $route.query.q }}</p>
     </div>
 </template>
 <script>
