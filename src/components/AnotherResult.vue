a5e2cf99dce17eef0d39ada3ac2dda57f9401030
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-21 15:44:22 -0400

    lesson 259 b

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -8,10 +8,15 @@
 <script>
     import {mapGetters } from 'vuex';
     export default {
-        computed: mapGetters({
-            doubleCounter: 'doubleCounter',
-            stringCounter: 'stringCounter'
-        })
+        computed: {
+            ...mapGetters({
+                doubleCounter: 'doubleCounter',
+                stringCounter: 'stringCounter'
+            }),
+            ourOwn() {
+                
+            }
+        }
         
     }
 </script>
