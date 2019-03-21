e64b3cfaea34b98a9f05042625ee0c3b47aff831
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-21 09:02:56 -0400

    update to base

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>Hello World!</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vue Udemy Template</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
