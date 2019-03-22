ab7a398bd0e74971873d0a659358bbde85263fb0
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-22 15:08:01 -0400

    base

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>Hello World!</h1>
+  <div id="app">
+    <app-header />
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-export default { }
+  import Header from './components/header/header.vue'
+  export default {
+    name: 'app',
+    components: {
+      'app-header': Header
+    }
+  }
 </script>
 
 <style>
-
+  body, html {
+    margin: 0;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
 </style>
