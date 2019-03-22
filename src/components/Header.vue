a57f7365e1dbed16c7feee5e43426f874b17e2a0
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-22 09:56:08 -0400

    lesson 290

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@
                 <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
                 <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
             </ul>
-            <strong class="navbar-text navbar-right" style="margin-right: 1px">Funds: {{ funds }}</strong>
+            <strong class="navbar-text navbar-right" style="margin-right: 1px">Funds: {{ funds | currency }}</strong>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">End Day</a></li>
                 <li class="dropdown">
