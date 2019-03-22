0cd701664c8b035406b3a3f51fa3aa28ae0874c5
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-22 15:28:03 -0400

    final update

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://vue-update.firebaseio.com'
+    baseURL: 'https://vue-update-73f39.firebaseio.com/'
 })
 
 instance.defaults.headers.common['SOMETHING'] = 'something'
