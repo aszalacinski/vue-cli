43be9e4fb017d0ccca4f2ccfb2871c53865b02af
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-22 14:36:27 -0400

    lesson 297

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,8 +14,8 @@
                 <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen" @mouseleave="isDropdownOpen = false">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Save & Load <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="">Save Data</a></li>
-                        <li><a href="">Load Data</a></li>
+                        <li><a href="#" @click="saveData">Save Data</a></li>
+                        <li><a href="#" @click="loadData">Load Data</a></li>
                     </ul>
                 </li>
             </ul>
@@ -38,10 +38,22 @@ export default {
     },
     methods: {
         ...mapActions({
-            randomizeStocks: 'randomizeStocks'
+            randomizeStocks: 'randomizeStocks',
+            fetchData: 'loadData'
         }),
         endDay() {
             this.randomizeStocks();
+        },
+        saveData() {
+            const data = {
+                funds: this.$store.getters.funds,
+                stockPortfolio: this.$store.getters.stockPortfolio,
+                stocks: this.$store.getters.stocks
+            };
+            this.$http.put('data.json', data);
+        },
+        loadData() {
+            this.fetchData();
         }
     }
 }
--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -22,35 +22,40 @@ const mutations = {
       state.stocks.splice(state.stocks.indexOf(record), 1);
     }
     state.funds += stockPrice * quantity;
+  },
+
+  SET_PORTFOLIO(state, portfolio) {
+    state.funds = portfolio.funds;
+    state.stocks = portfolio.stockPortfolio ? portfolio.stockPortfolio : [];
   }
 };
 
 const actions = {
-    sellStock: ({ commit }, order) => {
-        commit('SELL_STOCK', order);
-    }
+  sellStock: ({ commit }, order) => {
+    commit("SELL_STOCK", order);
+  }
 };
 
 const getters = {
-    stockPortfolio(state, getters) {
-        return state.stocks.map(stock => {
-            const record = getters.stocks.find(element => element.id == stock.id);
-            return {
-                id: stock.id,
-                quantity: stock.quantity,
-                name: record.name,
-                price: record.price
-            }
-        });
-    },
-    funds(state) {
-        return state.funds;
-    }
-}
+  stockPortfolio(state, getters) {
+    return state.stocks.map(stock => {
+      const record = getters.stocks.find(element => element.id == stock.id);
+      return {
+        id: stock.id,
+        quantity: stock.quantity,
+        name: record.name,
+        price: record.price
+      };
+    });
+  },
+  funds(state) {
+    return state.funds;
+  }
+};
 
 export default {
-    state,
-    mutations,
-    actions,
-    getters
-}
+  state,
+  mutations,
+  actions,
+  getters
+};
