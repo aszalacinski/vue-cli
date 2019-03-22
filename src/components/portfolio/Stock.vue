f18f1c8f0985de530b904e687b79036af0e8726b
Aarron Szalacinski <ASzalacinski@corporatespending.com>
2019-03-22 09:16:43 -0400

    lesson 287

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="col-sm-6 col-md-4">
-        <div class="panel panel-success">
+        <div class="panel panel-info">
             <div class="panel-heading">
                 <h3 class="panel-title">
                     {{stock.name}}
@@ -12,7 +12,7 @@
                     <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
                 </div>
                 <div class="pull-right">
-                    <button class="btn btn-success" @click="sellStock" :disabled="quantity <= 0 || !Number.isInteger(Number(quantity))">Sell</button>
+                    <button class="btn btn-info" @click="sellStock" :disabled="quantity <= 0 || !Number.isInteger(Number(quantity))">Sell</button>
                 </div>
             </div>
         </div>
@@ -29,7 +29,7 @@ export default {
     },
     methods: {
         ...mapActions({
-            sellStock: 'sellStock'
+            placeSellOrder: 'sellStock'
         }),
         sellStock() {
             const order = {
@@ -37,7 +37,8 @@ export default {
                 stockPrice: this.stock.price,
                 quantity: this.quantity
             };
-            this.sellStock();
+            this.placeSellOrder(order);
+            this.quantity = 0;
         }
     }
 }
